865494afe7977ec9ee753cdfe7171c95cb5d1eeb
ItsTato <ItsTato@gmail.com>
2021-12-20 03:01:40 -0600

    a

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Aod_PyHTML',
   packages = ['Aod_PyHTML'],
-  version = '0.3.4',
+  version = '0.3.5',
   license='GNU GPLv3',
   description = 'HTML but it\'s Python.',
   author = 'ItsTato',
   author_email = 'thatcoolcomputer@gmail.com',
   url = 'https://github.com/user/reponame',
-  download_url = 'https://github.com/ItsTato/PyHTML/archive/refs/tags/v0.3.4.tar.gz',
+  download_url = 'https://github.com/ItsTato/PyHTML/archive/refs/tags/v0.3.5.tar.gz',
   keywords = ['HTML', 'PYTHON-HTML', 'PYTHON HTML', 'AOD'],
   install_requires=[],
   classifiers=[
@@ -16,6 +16,6 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3',
   ],
 )
